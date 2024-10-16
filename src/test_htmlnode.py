1c3ea7e44388c721cdbd45cfaf91e9cf18c2874b
Kurlgargyey <nahsirkivar@gmail.com>
2024-10-16 16:11:37 +0200

    add LeafNode and tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
 	def test_fields(self):
@@ -17,5 +17,18 @@ class TestHTMLNode(unittest.TestCase):
 	def test_repr(self):
 		test_node = HTMLNode("a","A link",[])
 		self.assertEqual(f"{test_node}", "HTMLNode(a, A link, [], None)")
+
+	def test_leaf(self):
+		test_leaf = LeafNode("a", "A link", [], {"href": "lololol", "target":"_blank"})
+		self.assertTrue(test_leaf.children is None)
+
+	def test_leaf_repr(self):
+		test_leaf = LeafNode("a", "A link", [], {"href": "lololol", "target":"_blank"})
+		self.assertEqual(f"{test_leaf}", "LeafNode(a, A link, {'href': 'lololol', 'target': '_blank'})")
+
+	def test_leaf_needs_value(self):
+		test_leaf = LeafNode(value= None)
+		self.assertRaises(ValueError)
+
 if __name__ == "__main__":
 	unittest.main()
