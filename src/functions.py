6a4f6e162f916c6e5a7d9261db37e9298f0c721f
Kurlgargyey <nahsirkivar@gmail.com>
2024-12-13 14:03:43 +0100

    add list block conversion functionality

--- src/functions.py
+++ src/functions.py
@@ -41,12 +41,20 @@ def markdown_to_html_node(markdown):
 				root.children.append(HTMLNode("pre", children=[HTMLNode(tag, children=text_to_children(text))]))
 			case "quote":
 				tag = "quote"
-				text = map(lambda line: line.lstrip(">", block.split("\n"))).join()
+				text = map(lambda line: line.lstrip(">"), block.split("\n")).join()
 				root.children.append(HTMLNode(tag, children=text_to_children(text)))
 			case "unordered_list":
-				continue
+				list_node = HTMLNode("ul", children= [])
+				for item in block.split("\n"):
+					text = map(lambda line: line.lstrip("*+- "), item)
+					list_node.children.append(HTMLNode("li", children=text_to_children(text)))
+				root.children.append(list_node)
 			case "ordered_list":
-				continue
+				list_node = HTMLNode("ol", children= [])
+				for item in block.split("\n"):
+					text = map(lambda line: line.lstrip("123456789. "), item)
+					list_node.children.append(HTMLNode("li", children=text_to_children(text)))
+				root.children.append(list_node)
 			case "paragraph":
 				continue
 	return root
